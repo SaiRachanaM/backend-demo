46bea0dee31fded2016dbd11052f7005d7aaf6d0
Sai Rachana <sairachana.m@gmail.com>
2026-08-01 20:47:11 +0530

    Added dynamic port

--- server.js
+++ server.js
@@ -109,6 +109,8 @@ app.get("/profile", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
